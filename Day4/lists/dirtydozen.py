b0382e67b60cc02af99d929c842900d11f7cac55
Dharma1602 <dharmateja3103@gmail.com>
2024-03-01 09:46:46 -0500

    Rock paper scissors

---
+++ Day4/lists/dirtydozen.py
@@ -0,0 +1,17 @@
+# dirty_dozen = ["Straberries", "Spinach", "Kale", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Straberries", "Nectarains", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
+
+# fruits[-1] = "Melons"
+# fruits.append("Lemons")
+# print(fruits)
+
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+print(dirty_dozen[0][1])
+print(dirty_dozen[1][1])
